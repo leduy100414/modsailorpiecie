838ba8fc69e9ffb63e1cf8edd9822cdae4dce4e3
Lee Duy Anh <leduy909012@gmail.com>
2026-04-06 09:32:45 +0700

    Update speed.lua

--- speed.lua
+++ speed.lua
@@ -1,58 +1,47 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/leduy100414/modsailorpiecie/refs/heads/main/speed.lua"))()
--- =========================
--- ⚡ SPEED SYSTEM
--- =========================
-getgenv().PlayerSpeed = 16
-local player = game.Players.LocalPlayer
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
 
-local function UpdateSpeed()
-    local char = player.Character or player.CharacterAdded:Wait()
-    local humanoid = char:FindFirstChildOfClass("Humanoid")
-    if humanoid then
-        humanoid.WalkSpeed = getgenv().PlayerSpeed
-    end
-end
+       local Window = MakeWindow({
+         Hub = {
+         Title = "TBoy Roblox",
+         Animation = "Youtube: TBoy Roblox"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
 
--- Respawn giữ speed
-player.CharacterAdded:Connect(function()
-    task.wait(1)
-    UpdateSpeed()
-end)
-
--- Anti reset
-task.spawn(function()
-    while task.wait(0.5) do
-        pcall(UpdateSpeed)
-    end
-end)
-
--- =========================
--- 🎮 BUTTON TRONG TAB3o
--- =========================
-
-AddButton(Tab3o, {
-    Name = "⚡ Speed +10",
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=83190276951914",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Farm"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Redz Hub",
     Callback = function()
-        getgenv().PlayerSpeed += 10
-        UpdateSpeed()
-    end
-})
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
 
-AddButton(Tab3o, {
-    Name = "🐢 Speed -10",
-    Callback = function()
-        getgenv().PlayerSpeed -= 10
-        if getgenv().PlayerSpeed < 16 then
-            getgenv().PlayerSpeed = 16
-        end
-        UpdateSpeed()
-    end
-})
-
-AddButton(Tab3o, {
-    Name = "🔄 Reset Speed",
-    Callback = function()
-        getgenv().PlayerSpeed = 16
-        UpdateSpeed()
-    end
-})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+  end
+  })
